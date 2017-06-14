9770646519cad421df34ab7aba7ad9ea7eb6833f
Srikanth <srikanth@gatech.edu>
2017-06-14 11:10:50 -0700

    packet_recv: refactor - maintain current state separately

--- packet_recv.go
+++ packet_recv.go
@@ -10,21 +10,32 @@ import (
 )
 
 type Receiver struct {
-	E2eLatencies  []float64
 	PktChan       chan InputPkt
-	LocalHw       net.HardwareAddr
-	RemHw         net.HardwareAddr
 	LocalV4       net.IP
 	LocalV6       net.IP
-	SeqMap        map[uint32]int64
-	CurrSeq       uint32
-	CurrAck       uint32
-	LocalIp       net.IP
-	RemIp         net.IP
-	LocalPort     layers.TCPPort
-	RemPort       layers.TCPPort
 	SendStartChan chan bool
 	HasSentSend   bool
+	Curr          CurrStatus
+}
+
+type CurrStatus struct {
+	LocalIp      net.IP
+	RemIp        net.IP
+	LocalHw      net.HardwareAddr
+	RemHw        net.HardwareAddr
+	SeqMap       map[uint32]int64
+	TCPLocalIp   net.IP
+	TCPRemIp     net.IP
+	LocalPort    layers.TCPPort
+	RemPort      layers.TCPPort
+	Seq          uint32
+	Ack          uint32
+	Ip4          *layers.IPv4
+	Ip6          *layers.IPv6
+	Dir          int
+	IpDataLen    uint32
+	Transport    layers.IPProtocol
+	E2eLatencies []float64
 }
 
 const (
@@ -33,125 +44,136 @@ const (
 )
 
 func (r *Receiver) NewReceiver(pktChan chan InputPkt, hostV4 net.IP, hostV6 net.IP, sendStartChan chan bool) {
-	r.E2eLatencies = []float64{}
 	r.PktChan = pktChan
 	r.LocalV4 = hostV4
 	r.LocalV6 = hostV6
-	r.SeqMap = make(map[uint32]int64)
 	r.SendStartChan = sendStartChan
+
+	r.Curr = CurrStatus{}
+	r.Curr.E2eLatencies = []float64{}
+	r.Curr.SeqMap = make(map[uint32]int64)
 }
 
 func (r *Receiver) GetHardwareAddresses(pkt InputPkt) {
 	ethLayer := pkt.Packet.Layer(layers.LayerTypeEthernet)
 	eth, _ := ethLayer.(*layers.Ethernet)
-	r.LocalHw = eth.SrcMAC
-	r.RemHw = eth.DstMAC
+	r.Curr.LocalHw = eth.SrcMAC
+	r.Curr.RemHw = eth.DstMAC
 }
 
 func (r *Receiver) ParseTcpIn(pkt InputPkt, tcp *layers.TCP) {
 	pts := pkt.Packet.Metadata().Timestamp.UnixNano()
-	if sts, ok := r.SeqMap[tcp.Ack]; ok == true {
-		r.E2eLatencies = append(r.E2eLatencies, float64(pts-sts)/float64(time.Millisecond))
-		delete(r.SeqMap, tcp.Ack)
+	if sts, ok := r.Curr.SeqMap[tcp.Ack]; ok == true {
+		r.Curr.E2eLatencies = append(r.Curr.E2eLatencies, float64(pts-sts)/float64(time.Millisecond))
+		delete(r.Curr.SeqMap, tcp.Ack)
 	}
-	fmt.Printf("AAA %+v\n", r.E2eLatencies)
-	r.CurrAck = tcp.Seq
+	r.Curr.Ack = tcp.Seq
 	if tcp.SYN == true {
-		r.CurrAck++
+		r.Curr.Ack++
 	}
 }
 
-func (r *Receiver) ParseTcpOut(pkt InputPkt, tcp *layers.TCP, ipDataLen uint32) {
+func (r *Receiver) ParseTcpOut(pkt InputPkt, tcp *layers.TCP) {
 	if len(tcp.Payload) == 0 {
 		return
 	}
-	seq := tcp.Seq + ipDataLen - uint32(tcp.DataOffset*4)
-	r.SeqMap[seq] = pkt.Packet.Metadata().Timestamp.UnixNano()
-	r.CurrSeq = seq
+
+	seq := tcp.Seq + r.Curr.IpDataLen - uint32(tcp.DataOffset*4)
+	r.Curr.SeqMap[seq] = pkt.Packet.Metadata().Timestamp.UnixNano()
+	r.Curr.Seq = seq
 	if tcp.SYN == true {
-		r.CurrSeq++
+		r.Curr.Seq++
 	}
-	r.LocalPort = tcp.SrcPort
-	r.RemPort = tcp.DstPort
 	if r.HasSentSend == false && tcp.SYN == false {
 		r.GetHardwareAddresses(pkt)
+		r.Curr.TCPLocalIp = r.Curr.LocalIp
+		r.Curr.TCPRemIp = r.Curr.RemIp
+		r.Curr.LocalPort = tcp.SrcPort
+		r.Curr.RemPort = tcp.DstPort
+
 		r.SendStartChan <- true
 		r.HasSentSend = true
 	}
 }
 
-func (r *Receiver) ParseTcpLayer(pkt InputPkt, dir int, ipDataLen uint32) error {
+func (r *Receiver) ParseTcpLayer(pkt InputPkt) error {
 	tcpLayer := pkt.Packet.Layer(layers.LayerTypeTCP)
 	if tcpLayer == nil {
 		return errors.New("not a TCP pkt")
 	}
 
 	tcp, _ := tcpLayer.(*layers.TCP)
-	if dir == In {
+	if r.Curr.Dir == In {
 		r.ParseTcpIn(pkt, tcp)
 	} else {
-		r.ParseTcpOut(pkt, tcp, ipDataLen)
+		r.ParseTcpOut(pkt, tcp)
 	}
 	return nil
 }
 
-func (r *Receiver) ParseIcmpLayer(pkt InputPkt, dir int) error {
-	if dir == Out {
+func (r *Receiver) ParseIcmpLayer(pkt InputPkt) error {
+	fmt.Printf("AAAA %d %d\n", r.Curr.Transport, r.Curr.Dir)
+	if r.Curr.Dir == Out {
 		return errors.New("Outgoing ICMP")
 	}
 	if icmp4Layer := pkt.Packet.Layer(layers.LayerTypeICMPv4); icmp4Layer != nil {
-		ip, _ := pkt.Packet.Layer(layers.LayerTypeIPv4).(*layers.IPv4)
-		fmt.Printf("ICMP Pkt from %s\n", ip.SrcIP.String())
+		r.Curr.Ip4, _ = pkt.Packet.Layer(layers.LayerTypeIPv4).(*layers.IPv4)
+		_, _ = icmp4Layer.(*layers.ICMPv4)
+		fmt.Printf("ICMP Pkt from %s\n", r.Curr.Ip4.SrcIP.String())
+	}
+	//TODO: ICMPv6
+	return nil
+}
+
+func (r *Receiver) ParseIpLayer(pkt InputPkt) error {
+	if ip4Layer := pkt.Packet.Layer(layers.LayerTypeIPv4); ip4Layer != nil {
+		r.Curr.Ip4 = ip4Layer.(*layers.IPv4)
+		r.Curr.Ip6 = nil
+		r.Curr.IpDataLen = uint32(r.Curr.Ip4.Length - 4*uint16(r.Curr.Ip4.IHL))
+		if r.Curr.Ip4.DstIP.String() == r.LocalV4.String() {
+			r.Curr.Dir = In
+			r.Curr.LocalIp = r.Curr.Ip4.DstIP
+			r.Curr.RemIp = r.Curr.Ip4.SrcIP
+		} else {
+			r.Curr.Dir = Out
+			r.Curr.LocalIp = r.Curr.Ip4.SrcIP
+			r.Curr.RemIp = r.Curr.Ip4.DstIP
+		}
+		r.Curr.Transport = r.Curr.Ip4.Protocol
+	} else if ip6Layer := pkt.Packet.Layer(layers.LayerTypeIPv6); ip6Layer != nil {
+		r.Curr.Ip6 = ip6Layer.(*layers.IPv6)
+		r.Curr.Ip4 = nil
+		r.Curr.IpDataLen = uint32(r.Curr.Ip6.Length)
+		if r.Curr.Ip6.DstIP.String() == r.LocalV6.String() {
+			r.Curr.Dir = In
+			r.Curr.LocalIp = r.Curr.Ip6.DstIP
+			r.Curr.RemIp = r.Curr.Ip6.SrcIP
+		} else {
+			r.Curr.Dir = Out
+			r.Curr.LocalIp = r.Curr.Ip6.SrcIP
+			r.Curr.RemIp = r.Curr.Ip6.DstIP
+		}
+		r.Curr.Transport = r.Curr.Ip6.NextHeader
+
+	} else {
+		return errors.New("Not IP")
 	}
 	return nil
 }
 
-func (r *Receiver) ParseIpLayer(pkt InputPkt) (int, layers.IPProtocol, uint32, error) {
-	dir := Out
-	var ipDataLen uint32
-	var transport layers.IPProtocol
-
-	if ip4Layer := pkt.Packet.Layer(layers.LayerTypeIPv4); ip4Layer != nil {
-		ip, _ := ip4Layer.(*layers.IPv4)
-		r.LocalIp = ip.SrcIP
-		r.RemIp = ip.DstIP
-		ipDataLen = uint32(ip.Length - 4*uint16(ip.IHL))
-		if ip.DstIP.String() == r.LocalV4.String() {
-			dir = In
-			r.LocalIp = ip.DstIP
-			r.RemIp = ip.SrcIP
-		}
-		transport = ip.Protocol
-	} else if ip6Layer := pkt.Packet.Layer(layers.LayerTypeIPv6); ip6Layer != nil {
-		ip, _ := ip6Layer.(*layers.IPv6)
-		r.LocalIp = ip.SrcIP
-		r.RemIp = ip.DstIP
-		ipDataLen = uint32(ip.Length)
-		if ip.DstIP.String() == r.LocalV6.String() {
-			dir = In
-			r.LocalIp = ip.DstIP
-			r.RemIp = ip.SrcIP
-		}
-		transport = ip.NextHeader
-
-	} else {
-		return 0, 0, 0, errors.New("Not IP")
-	}
-	return dir, transport, ipDataLen, nil
-}
-
 func (r *Receiver) Run() {
+	fmt.Printf("Starting receiver\n")
 	for {
 		pkt := <-r.PktChan
-		dir, transport, ipDataLen, err := r.ParseIpLayer(pkt)
+		err := r.ParseIpLayer(pkt)
 		if err != nil {
 			continue
 		}
 		switch {
-		case transport == layers.IPProtocolTCP:
-			r.ParseTcpLayer(pkt, dir, ipDataLen)
-		case transport == layers.IPProtocolICMPv4 || transport == layers.IPProtocolICMPv6:
-			r.ParseIcmpLayer(pkt, dir)
+		case r.Curr.Transport == layers.IPProtocolTCP:
+			r.ParseTcpLayer(pkt)
+		case r.Curr.Transport == layers.IPProtocolICMPv4 || r.Curr.Transport == layers.IPProtocolICMPv6:
+			r.ParseIcmpLayer(pkt)
 		}
 	}
 }
