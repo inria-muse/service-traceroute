ee8fd44036c25a1970f4a51fc1351b652fe644d9
Srikanth <srikanth@gatech.edu>
2017-06-20 12:10:47 -0700

    buffer_trace: analyze pkts sent by receiver.
    for flow pkts, just note timestamp and map to seq/ack.
    
    for icmp outgoing, note the ipid, for incoming, parse the icmp payload
    to extract ipid.
    
    TODO: use tcp seq for icmp as well.

--- buffer_trace.go
+++ buffer_trace.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/binary"
 	"fmt"
 	"time"
 
@@ -9,15 +10,22 @@ import (
 )
 
 type BufferTrace struct {
-	SendQ    chan []gopacket.SerializableLayer
-	R        *Receiver
-	DoneSend chan bool
-	OutChan  chan string
+	SendQ        chan []gopacket.SerializableLayer
+	R            *Receiver
+	FlowSeqMap   map[uint32]int64
+	ProbeIdMap   map[uint16]int64
+	ProbeSeqMap  map[uint16]int64
+	E2eLatencies []float64
+	DoneSend     chan bool
+	OutChan      chan string
 }
 
 func (bt *BufferTrace) NewBufferTrace(r *Receiver, sendQ chan []gopacket.SerializableLayer, outChan chan string) {
-	bt.R = r
 	bt.SendQ = sendQ
+	bt.R = r
+	bt.FlowSeqMap = make(map[uint32]int64)
+	bt.ProbeIdMap = make(map[uint16]int64)
+	bt.E2eLatencies = []float64{}
 	bt.DoneSend = make(chan bool)
 	bt.OutChan = outChan
 }
@@ -52,7 +60,6 @@ func (bt *BufferTrace) SendPkts() {
 		Window:     0xffff,
 	}
 	for i := 0; i < 20; i++ {
-		bt.OutChan <- fmt.Sprintf("%.3f: Sending packet %d", float64(time.Now().UnixNano())/float64(time.Millisecond), i)
 		ipLayer.TTL = uint8(i + 1)
 		ipLayer.Id = uint16(ipLayer.TTL)
 		tcpLayer.Seq = bt.R.Curr.Seq
@@ -64,8 +71,32 @@ func (bt *BufferTrace) SendPkts() {
 	bt.DoneSend <- true
 }
 
+func (bt *BufferTrace) AnalyzePackets() {
+	bt.OutChan <- fmt.Sprintf("%.3f Experiment analysis engine", float64(time.Now().UnixNano())/float64(time.Millisecond))
+	for {
+		select {
+		case c := <-bt.R.FlowOutChan:
+			bt.FlowSeqMap[c.Seq+c.IpDataLen-c.TcpHLen] = c.Ts
+		case c := <-bt.R.FlowInChan:
+			if sts, ok := bt.FlowSeqMap[c.Ack]; ok == true {
+				bt.E2eLatencies = append(bt.E2eLatencies, float64(c.Ts-sts)/float64(time.Millisecond))
+				delete(bt.FlowSeqMap, c.Ack)
+			}
+		case c := <-bt.R.ProbeOutChan:
+			bt.ProbeIdMap[c.IpId] = c.Ts
+		case c := <-bt.R.ProbeInChan:
+			var id uint16 = binary.BigEndian.Uint16(c.IcmpPayload[4:6])
+			if oTs, ok := bt.ProbeIdMap[id]; ok {
+				bt.OutChan <- fmt.Sprintf("Probe latency from %s (hop %d): %.3f", c.RemIp.String(), id, float64(c.Ts-oTs)/float64(time.Millisecond))
+				delete(bt.ProbeIdMap, id)
+			}
+		}
+	}
+}
+
 func (bt *BufferTrace) Run() {
 	bt.OutChan <- fmt.Sprintf("%.3f: Starting buffertrace experiment", float64(time.Now().UnixNano())/float64(time.Millisecond))
+	go bt.AnalyzePackets()
 	go bt.SendPkts()
 	<-bt.DoneSend
 }
